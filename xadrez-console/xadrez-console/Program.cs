0d140cf1234d6f5b07de9ad97a12b043cdbb4054
Fernando <fernandobarrosdesak@gmail.com>
2025-01-21 15:57:00 -0300

    primeiros moviementos das pecas

--- xadrez-console/xadrez-console/Program.cs
+++ xadrez-console/xadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using tabuleiro;
+using xadrez_console.tabuleiro;
 using xadrez_console.xadrez;
 
 namespace xadrez_console
@@ -7,20 +8,28 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-            tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
-
-
-            Tela.imprimirTabuleiro(tab);
-
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                    
+                    partida.executaMovimento(origem, destino);
+                }
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
-
         }
     }
 }
